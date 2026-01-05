20e383ced9c92eb3573fe39682a70aa5054e7795
AntheaHuang <ju.anthea.huang@gmail.com>
2026-01-04 23:34:13 -0800

    add decimal to rating

--- utils/actions.ts
+++ utils/actions.ts
@@ -386,7 +386,7 @@ export async function fetchPropertyRating(propertyId: string) {
     },
   });
   return {
-    rating: result[0]?._avg.rating?.toFixed() ?? 0,
+    rating: result[0]?._avg.rating?.toFixed(2) ?? 0,
     count: result[0]?._count.rating ?? 0,
   };
 }
