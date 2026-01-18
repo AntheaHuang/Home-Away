49ca706dc21f08f4e83a8181fcddecdfac80f984
AntheaHuang <ju.anthea.huang@gmail.com>
2026-01-17 18:33:01 -0800

    add realtime countdown for pending booking reminder

--- components/booking/BookingReminder.tsx
+++ components/booking/BookingReminder.tsx
@@ -1,14 +1,75 @@
+"use client";
+
 import { hasPendingBooking } from "@/utils/actions";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
-async function BookingReminder() {
-  const bookings = await hasPendingBooking();
+type pendingBooking = {
+  id: string;
+  expiresAt: Date;
+};
+function BookingReminder() {
+  const [bookings, setBookings] = useState<pendingBooking[]>([]);
+  const [visible, setVisible] = useState(true);
+  const [remainingSeconds, setRemainingSeconds] = useState<number>(0);
 
-  if (bookings.length === 0) return null;
+  async function fetchPendingBookings() {
+    const pendingBookings: pendingBooking[] = await hasPendingBooking();
+
+    if (pendingBookings.length === 0) {
+      setVisible(false);
+    } else {
+      setBookings(pendingBookings);
+
+      // Use the earliest expiry for countdown
+      // Will avoid multiple pending bookings in future implementation
+      const earliestExpiry = Math.min(
+        ...pendingBookings.map((booking) => booking.expiresAt.getTime())
+      );
+      setRemainingSeconds(Math.floor((earliestExpiry - Date.now()) / 1000));
+    }
+  }
+
+  useEffect(() => {
+    fetchPendingBookings();
+
+    // refetch every 5 seconds
+    const interval = setInterval(() => {
+      fetchPendingBookings();
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  // Countdown timer
+  useEffect(() => {
+    if (!visible || remainingSeconds <= 0) return;
+
+    const interval = setInterval(() => {
+      setRemainingSeconds((prev) => {
+        if (prev <= 1) {
+          clearInterval(interval);
+          setVisible(false);
+          return 0;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [remainingSeconds, visible]);
+
+  if (!visible || bookings.length === 0) return null;
+
+  const minutes = Math.floor(remainingSeconds / 60);
+  const seconds = remainingSeconds % 60;
 
   return (
     <div className="fixed bottom-6 left-1/2 -translate-x-1/2 py-5 px-8 rounded-full bg-primary text-primary-foreground font-medium flex gap-8 items-center">
-      <p>You have a pending booking</p>
+      <p>
+        You have a pending booking which will expire in {minutes}:
+        {seconds.toString().padStart(2, "0")}
+      </p>
       <Link href={`/bookings`} className="underline tracking-wide font-light">
         View Details
       </Link>
