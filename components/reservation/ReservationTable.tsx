ed299edb2907c32e558b2931fa9d5ef77f988173
AntheaHuang <ju.anthea.huang@gmail.com>
2026-01-29 22:27:16 -0800

    finish review modal ui

--- components/reservation/ReservationTable.tsx
+++ components/reservation/ReservationTable.tsx
@@ -2,6 +2,7 @@
 
 import CountryFlagAndName from "@/components/card/CountryFlagAndName";
 import Link from "next/link";
+import { Modal } from "antd";
 
 import { formatDate, formatCurrency } from "@/utils/format";
 import {
@@ -14,6 +15,10 @@ import {
 } from "@/components/ui/table";
 
 import { TableInfo } from "@/utils/types";
+import { useState } from "react";
+import RatingInput from "../form/RatingInput";
+import TextAreaInput from "../form/TextAreaInput";
+import { Button } from "../ui/button";
 
 export default function ReservationTable({
   reservations,
@@ -22,6 +27,8 @@ export default function ReservationTable({
   reservations: TableInfo[];
   isPast?: boolean;
 }) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   if (reservations.length === 0) {
     return;
   }
@@ -78,11 +85,43 @@ export default function ReservationTable({
                 {isPast && (
                   <TableCell>
                     <p
-                      // onClick={() => openReviewModal()}
-                      className="underline text-primary tracking-wide"
+                      onClick={() => setIsModalVisible(true)}
+                      className="underline text-primary tracking-wide cursor-pointer"
                     >
                       Leave a review for {firstName}
                     </p>
+                    <Modal
+                      title={`Leave your review for ${firstName}`}
+                      open={isModalVisible}
+                      footer={null}
+                      onCancel={() => setIsModalVisible(false)}
+                    >
+                      <input
+                        type="hidden"
+                        name="propertyId"
+                        value={propertyId}
+                      />
+                      <RatingInput name="rating" />
+                      <TextAreaInput
+                        name="comment"
+                        labelText="Comment"
+                        defaultValue="Nice guests"
+                      />
+                      <footer className="flex justify-end gap-8">
+                        <Button
+                          className="px-4 py-2 rounded"
+                          onClick={() => setIsModalVisible(false)}
+                        >
+                          Cancel
+                        </Button>
+                        <Button
+                          className="px-4 py-2 rounded"
+                          onClick={() => setIsModalVisible(false)}
+                        >
+                          Submit
+                        </Button>
+                      </footer>
+                    </Modal>
                   </TableCell>
                 )}
               </TableRow>
