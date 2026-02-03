44e5211c6eeffca9c1db4ef3f8d7f3f69a543682
AntheaHuang <ju.anthea.huang@gmail.com>
2026-02-02 16:23:24 -0800

    improve booking page ui

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,6 +1,7 @@
 import { fetchBookings, deleteBookingAction } from "@/utils/actions";
 import BookingTable from "@/components/booking/BookingTable";
 import { TableInfo } from "@/utils/types";
+import EmptyList from "@/components/home/EmptyList";
 
 async function BookingsPage() {
   const bookings: TableInfo[] = await fetchBookings();
@@ -14,13 +15,26 @@ async function BookingsPage() {
   );
 
   return (
-    <div className="mt-16">
-      <h4 className="mb-4 capitalize">
-        active bookings : {activeBookings.length}
-      </h4>
-      <BookingTable bookings={activeBookings} />
-      <h4 className="mb-4 capitalize">past bookings : {pastBookings.length}</h4>
-      <BookingTable bookings={pastBookings} />
+    <div className="mt-4">
+      <div className="mb-8">
+        {activeBookings.length === 0 ? (
+          <EmptyList heading="You have no active booking" />
+        ) : (
+          <h4 className="mb-4 font-bold text-lg capitalize">
+            active bookings: {activeBookings.length}
+          </h4>
+        )}
+        <BookingTable bookings={activeBookings} />
+      </div>
+
+      {pastBookings.length > 0 && (
+        <div>
+          <h4 className="mb-4 font-bold text-lg capitalize">
+            past bookings : {pastBookings.length}
+          </h4>
+          <BookingTable bookings={pastBookings} />
+        </div>
+      )}
     </div>
   );
 }
